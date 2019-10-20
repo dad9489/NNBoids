556e70264839aa91df6a773e3dbb150bb9933ae5
David <dad9489@rit.edu>
2019-10-20 01:05:20 -0400

    second camera

---
+++ Assets/Scripts/GlobalManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalManager : MonoBehaviour {
+    public static GlobalManager Instance { get; set; }
+
+    public BoidAgent selectedAgent = null;
+
+    private void Awake() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
